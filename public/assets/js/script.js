0b499eacc9fafece52d691f4b2cf9b4d98a8cbc2
Sean Kempf <poserposer@me.com>
2020-11-10 18:30:55 -0700

    Created ajax calls to the apis to do the PUTs, POSTs, and DELETEs

---
+++ public/assets/js/script.js
@@ -0,0 +1,45 @@
+$(function(){
+    $(".change-devoured").on("click", function(e){
+        const id = $(this).data("id");
+        const newDevoured = $(this).data("newDevoured");
+
+        const newDevouredState= {
+            devour: newDevoured
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT", 
+            data: newDevouredState
+        }).then(function(){
+
+            location.reload();
+        })
+    });
+
+    $(".create-form").on("submit", function(e){
+        e.preventDefault();
+        
+        const newBurger = {
+            name: $("#burger").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+        $.ajax("/api/burgers", {
+            type: "POST", 
+            data: newBurger
+        }).then(function (){
+
+            location.reload()
+        })
+    });
+
+    $(".delete-burger").on("click", function(e){
+        const id = $(this).data("id");
+
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(function(){
+
+            location.reload();
+        })
+    });
+});
